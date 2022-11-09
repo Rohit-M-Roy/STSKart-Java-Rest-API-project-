6b3ad9a5f6b29a539c8bb01b02ac34a03815eee0
Yash Bhatter <yashbhatter.yb@gmail.com>
2022-11-10 00:02:38 +0530

    Packages

---
+++ STSKart/src/main/java/com/Shopping/StsKartApplication.java
@@ -0,0 +1,13 @@
+package com.Shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsKartApplication.class, args);
+	}
+
+}
