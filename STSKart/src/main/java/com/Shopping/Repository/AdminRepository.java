83b4f1625c95fb7419529fbd1c29fa04e183881d
Humam <104218563+humamul@users.noreply.github.com>
2022-11-10 13:28:05 +0530

    Update AdminRepository.java

--- STSKart/src/main/java/com/Shopping/Repository/AdminRepository.java
+++ STSKart/src/main/java/com/Shopping/Repository/AdminRepository.java
@@ -1,4 +1,4 @@
-package com.shopping.repository;
+package com.Shopping.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
