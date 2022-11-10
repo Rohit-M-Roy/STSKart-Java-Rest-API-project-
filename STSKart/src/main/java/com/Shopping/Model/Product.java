856b560f7c27b581f3ddb3da819da55ab1cbf2f1
R Sai Datta Reddy <saidattareddy007@gmail.com>
2022-11-10 13:04:52 +0530

    Initial Product Bean

---
+++ STSKart/src/main/java/com/Shopping/Model/Product.java
@@ -0,0 +1,32 @@
+package com.Shopping.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer productId ;
+	
+	private String productName;
+	private Double price;
+	private String color;
+	private String dimension;
+	private String specification;
+	private String manufacturer;
+	private Integer quantity;
+	private String category;
+	
+
+}
